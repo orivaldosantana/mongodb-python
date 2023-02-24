6bcce42f21d31eb964de4895645a41fb6c81cd59
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-02-23 22:59:08 -0300

    inserindo turmas

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mongodb-python
+
 Mongodb com Python
+
+## Referências
+
+- [Using MongoDB with Docker](https://earthly.dev/blog/mongodb-docker/)
+- [MongoDB Shell](https://www.mongodb.com/docs/mongodb-shell/)
+- [Connecting IP address not in IP Access List](https://www.mongodb.com/docs/atlas/troubleshoot-connection/)
---
+++ write_classes.py
@@ -0,0 +1,22 @@
+from dotenv import dotenv_values
+import pymongo
+from pymongo.server_api import ServerApi
+
+config = dotenv_values(".env")
+
+print(config['ATLAS_URI']) 
+
+client = pymongo.MongoClient(config['ATLAS_URI'], server_api=ServerApi('1'))
+db = client['dataviewert1'] 
+classesCollections = db['classes']
+
+
+
+import json 
+
+with open('./dados/json_turmas.json', mode='r',  encoding=" UTF-8") as classes_file:
+  classesData = json.load(classes_file) 
+  classesCollections.insert_many(classesData)  
+  for c in classesData:
+    print('\n - - - ') 
+    print(c) 
