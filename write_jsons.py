e0b6ff4fa6462885295be105110172e607a57236
Orivaldo Santana Jr <orivaldo@gmail.com>
2023-03-22 15:23:59 -0300

    teste youtube

---
+++ write_jsons.py
@@ -0,0 +1,27 @@
+from dotenv import dotenv_values
+import pymongo
+from pymongo.server_api import ServerApi
+import json 
+
+config = dotenv_values(".env")
+
+client = pymongo.MongoClient(config['ATLAS_URI'], server_api=ServerApi('1'))
+db = client['dataviewert1'] 
+
+
+def insertManyJsons(db, nameJsonFile, nameCollection):
+  collections = db[nameCollection]
+  with open(nameJsonFile, mode='r',  encoding=" UTF-8") as json_file:
+    collectionsData = json.load(json_file) 
+    try: 
+      collections.insert_many(collectionsData)  
+      cont = 1
+      for c in collectionsData:
+        print('\n - - - ',cont) 
+        print(c) 
+        cont = cont + 1 
+    except:
+      print("Erro ao inserir no banco de dados!")  
+
+
+#insertManyJsons(db,'./dados/prof_turmas.json','prof_classes') 
